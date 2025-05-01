e7051d0a26d874ac7f0de892c37d007b9d17e0e5
Zvelocity <intimidation1@hotmail.com>
2025-05-01 18:13:11 -0400

    init view for tui, added tview dependencies

--- main.go
+++ main.go
@@ -1,78 +1,25 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	"github.com/rivo/tview"
 )
 
-type model struct {
-	pods    []string
-	message string
-}
-
-func (m model) Init() tea.Cmd {
-	return fetchPods
-}
-
-func fetchPods() tea.Msg {
-	kubeconfig := os.Getenv("KUBECONFIG") // or use the default path if you dont have env variable set
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		return err
-	}
-
-	clientset, err := kubernetes.NewForConfig(config) // create a new clientset
-	if err != nil {
-		return err
-	}
-
-	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), v1.ListOptions{}) // get all pods in the cluster
-	if err != nil {
-		return err
-	}
-
-	var podNames []string
-	for _, pod := range pods.Items {
-		podNames = append(podNames, pod.Name)
-	}
-	return podNames
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		}
-	case []string:
-		m.pods = msg
-	case error:
-		m.message = fmt.Sprintf("Error: %v", msg)
-	}
-	return m, nil
-}
-
-func (m model) View() string { // view function to render the UI
-	view := "Hello, Kubernetes!\n\nPods:\n"
-	for _, pod := range m.pods { // loop through the pods and display them
-		view += fmt.Sprintf("- %s\n", pod)
-	}
-	view += "\nPress Ctrl+C or 'q' to quit.\n"
-	return view
-}
-
 func main() {
-	p := tea.NewProgram(model{message: "Welcome to your TUI!"})
-	_, err := p.Run()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error starting program: %v\n", err)
-		os.Exit(1)
+	app := tview.NewApplication()
+
+	// Create a simple text view to start
+	textView := tview.NewTextView().
+		SetText("K8s TUI!\n").
+		SetTextAlign(tview.AlignCenter).
+		SetDynamicColors(true)
+
+	// Create a flex layout to hold our components
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(textView, 0, 1, true)
+
+	// Run the application
+	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
 	}
 }
