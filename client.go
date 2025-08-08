e169f51fed9cec904ec5d20f16dc5180de6f4064
Zvelocity <intimidation1@hotmail.com>
2025-08-08 00:54:04 -0400

    setup k8s client to pull kubeconfig from home dir

---
+++ client.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+// initK8sClient initializes the Kubernetes client
+func initK8sClient() (*kubernetes.Clientset, error) {
+	var kubeconfig string
+
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = filepath.Join(home, ".kube", "config")
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build kubeconfig: %w", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create kubernetes client: %w", err)
+	}
+
+	return clientset, nil
+}
